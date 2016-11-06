6113553282cd80aa094882213d3b8243829a957a
emladinov <emladinov01@manhattan.edu>
2016-11-06 17:53:39 -0500

    - Added login screen - Submit button works with correct password (currently test) - made new activity to list applications that will have passwords. - will use listview??

--- app/src/main/java/com/example/evan/markandevance/PasswordEntry.java
+++ app/src/main/java/com/example/evan/markandevance/PasswordEntry.java
@@ -1,7 +1,14 @@
 package com.example.evan.markandevance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.Console;
+
+import static com.example.evan.markandevance.R.id.passEntry;
 
 public class PasswordEntry extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class PasswordEntry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_entry);
+    }
+
+    public void onClick(View view) {
+        EditText objEdit;
+      //  if passtext == entry;
+      //  go to page
+        objEdit = (EditText)findViewById(R.id.passEntry);
+        String text = objEdit.getText().toString();
+        if (text.equals("test")) {
+            Intent intent = new Intent(this, ApplicationList.class);
+            startActivity(intent);
+        }
     }
 }
