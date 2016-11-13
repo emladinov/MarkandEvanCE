c37eba6e0e6380039bac345f1321fcb059340473
Mark <mangioli01@manhattan.edu>
2016-11-13 16:43:36 -0500

    Made the ListView work

--- app/src/main/java/com/example/evan/markandevance/ApplicationList.java
+++ app/src/main/java/com/example/evan/markandevance/ApplicationList.java
@@ -2,12 +2,24 @@ package com.example.evan.markandevance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ApplicationList extends AppCompatActivity {
-
+    private ListView mainListView;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application_list);
+        mainListView = (ListView) findViewById(R.id.listofApps);
+        String [] accounts = new String[] {"Amazon", "Dropbox","Google"};
+        ArrayList<String> websites = new ArrayList<String>();
+        websites.addAll(Arrays.asList(accounts));
+        adapter = new ArrayAdapter<String>(this, R.layout.rowlayout, websites);
+        mainListView.setAdapter(adapter);
     }
 }
