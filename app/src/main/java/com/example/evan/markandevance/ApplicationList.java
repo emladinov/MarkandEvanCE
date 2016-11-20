d4b1e7b62068fa40385ef76d6da49c9e827e63eb
emladinov <emladinov01@manhattan.edu>
2016-11-19 19:56:20 -0500

    urlconnectino line is not working.

--- app/src/main/java/com/example/evan/markandevance/ApplicationList.java
+++ app/src/main/java/com/example/evan/markandevance/ApplicationList.java
@@ -4,7 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.content.Context;
+import android.os.AsyncTask;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,10 +28,46 @@ public class ApplicationList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application_list);
         mainListView = (ListView) findViewById(R.id.listofapps);
-        String [] accounts = new String[] {"Amazon", "Dropbox","Google"};
+        String data_url = "http://149.61.165.155";
+        String [] accounts = new String[50];
+        try {
+            URL url = new URL(data_url);
+            System.out.println("It works");
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setDoInput(true);
+            //OutputStream outputStream = httpURLConnection.getOutputStream();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+            String line = "";
+            int i = 0;
+
+            while((line = bufferedreader.readLine()) != null){
+                accounts[i] += line;
+                i++;
+            }
+
+
+            bufferedreader.close();
+            inputStream.close();
+            httpURLConnection.disconnect();
+
+            System.out.println(accounts[1]);
+        }
+         catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+
         ArrayList<String> websites = new ArrayList<String>();
         websites.addAll(Arrays.asList(accounts));
         adapter = new ArrayAdapter<String>(this, R.layout.rowlayout, websites);
         mainListView.setAdapter(adapter);
     }
 }
+
